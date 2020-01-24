99f9b0ef0fdfad651a6ddd1fcf2920820e344e07
oscarlarios7a <javier.atoaxaca@gmail.com>
2020-01-24 00:15:27 -0600

    Commit Trabajo con Routes/ExpresionesRegulares/NameRoute

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,6 @@
+<!--- Aqui se esta utilizando con el metodo de url --!-->
+<!--<a href="{{url('admin/sistema/permiso')}}">Ir a Permisos </a>-- !>
+
+< !-- Aqui se esta utilizando con el metodo de route --!-->
+<a href="{{route('permiso')}}">Ir a Permisos </a>
+
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 /*
 |--------------------------------------------------------------------------
@@ -10,7 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+//Ruta Principal
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+//Estoy pasando a traves de la Url el nombre o el dato 'permiso/{nombre}' es obligatorio, con el signo ?'permiso/{nombre?}' es opcional
+//asi con el 'PermisoController@index' estoy accediendo al nombre del controlador('PermisoController)anexando el metodo (@index')
+//Route::get('permiso/{nombre}/{slug?}', 'PermisoController@index');
+//Dando un nombre a la ruta para acceder al controlador
+//Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso');
+//Dando un nombre a la ruta con una expresion Regular
+Route::get('permiso/{nombre}/{id?}', function($nombre,$id=false){
+    return $nombre.'-'.$id;
+})->where( 'nombre','[A-Za-z]+')->where('id','[0-9]+')->name('permiso');
