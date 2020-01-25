08115c57bd556ba8f1502af99120af3605748fd6
oscarlarios7a <javier.atoaxaca@gmail.com>
2020-01-24 23:05:36 -0600

    Commit Correcion en crear.blade.php y PermisoController@crear

--- resources/views/admin/permiso/crear.blade.php
+++ resources/views/admin/permiso/crear.blade.php
@@ -2,7 +2,7 @@
 @extends("theme.$theme.layout")
 <!-- Aqui estoy colocando un titulo a la vista muy diferente a cuando estoy en la Rais del Proyecto-->
 @section('titulo')
-    Crear Permisos
+    Permisos
 @endsection
 <!-- Aqui estoy extendiendo la vista enuna seccion de la vista de view/theme/lte/layout.blade.php -->
 
@@ -13,27 +13,8 @@
             <div class="box-header with-border">
               <h3 class="box-title">Seccion de Crear Permisos </h3>
             </div>
-            <div class="box-body table-responsive no-padding">
-                <table class="table table-bordered table-hover table-striped">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Nombre</th>
-                            <th>Slug</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($permisos as $permiso)
-                            <tr>
-                                <td>{{$permiso->id}}</td>
-                                <td>{{$permiso->nombre}}</td>
-                                <td>{{$permiso->slug}}</td>
-                                <td></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            <div class="box-body ">
+
             </div>
         </div>
     </div>
